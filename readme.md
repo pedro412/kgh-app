1d82e8e3d50010f7ca8979c6e1bc6833419ab799
Pedro Alvarez <pedro.alvarez412@outlook.com>
2021-07-07 22:28:10 -0500

    add port for heroku and readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Deploy to Heroku
+
+App will be hosted on heroku, to deploy go to Heroku page.
+
+## Env vars
+
+Env vars should be added/changed on heroku web, for local dev use .env file, refer to template
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const limiter = rateLimit({
